c1aa178c35be3d90af4f780771739c70def532a5
Volodymyr Lapan <volodymyr.lapan@wework.com>
2023-05-05 14:34:10 +0200

    Install tailwind css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </React.StrictMode>
 );
 
